fe82cec9c3b6c8c286c2e8ae401b7e1ab6a5112f
Andrey Helldar <helldar@dragon-code.pro>
2026-01-01 22:14:50 +0300

    Add `UserMachine` pivot model, migration, and factory

---
+++ database/factories/UserMachineFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Machine;
+use App\Models\Nozzle;
+use App\Models\User;
+use App\Models\UserMachine;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class UserMachineFactory extends Factory
+{
+    protected $model = UserMachine::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id'    => User::factory(),
+            'machine_id' => Machine::factory(),
+            'nozzle_id'  => Nozzle::factory(),
+
+            'filament_load_time'   => $this->faker->randomNumber(),
+            'filament_unload_time' => $this->faker->randomNumber(),
+            'tool_change_time'     => $this->faker->randomNumber(),
+
+            'can_change_filament' => $this->faker->boolean(),
+
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+
+        ];
+    }
+}
