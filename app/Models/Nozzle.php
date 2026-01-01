c5819fb17258ce75925f79e9e1305bc85ef5a8fe
Andrey Helldar <helldar@dragon-code.pro>
2026-01-01 21:43:51 +0300

    Add `Nozzle` model, migration, and factory

---
+++ app/Models/Nozzle.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Nozzle extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'slug',
+        'title',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'title' => 'decimal',
+        ];
+    }
+}
---
+++ database/factories/NozzleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Nozzle;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class NozzleFactory extends Factory
+{
+    protected $model = Nozzle::class;
+
+    public function definition(): array
+    {
+        return [
+            'slug'       => $this->faker->slug(),
+            'title'      => $this->faker->randomFloat(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
