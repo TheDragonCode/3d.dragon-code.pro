49863975bdeb41ee26252ef98046cff8990b58b3
Andrey Helldar <helldar@dragon-code.pro>
2026-01-02 14:23:55 +0300

    Add `DownloadOrcaSlicerCommand` and expand `OrcaSlicerService` with download, extract, and cleanup functionality

---
+++ app/Console/Commands/DownloadOrcaSlicerCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Services\OrcaSlicerService;
+use Illuminate\Console\Command;
+
+class DownloadOrcaSlicerCommand extends Command
+{
+    protected $signature = 'download:orca-slicer';
+
+    protected $description = 'Command description';
+
+    public function handle(OrcaSlicerService $service): void
+    {
+        $this->components->task('Clean up', fn () => $service->cleanup());
+        $this->components->task('Download', fn () => $service->download());
+        $this->components->task('Extract', fn () => $service->extract());
+    }
+}
