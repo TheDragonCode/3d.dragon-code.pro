f52355d10569988c8b746dc683b60f2b263b3fcc
Andrey Helldar <helldar@dragon-code.pro>
2026-01-02 14:55:33 +0300

    Refactor `TranslatorService`: extract locale file path logic, improve translation handling

--- app/Services/OrcaSlicer/TranslatorService.php
+++ app/Services/OrcaSlicer/TranslatorService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services\OrcaSlicer;
 
 use Gettext\Loader\PoLoader;
+use Gettext\Translations;
 use Illuminate\Container\Attributes\Config;
 use Illuminate\Container\Attributes\Storage;
 use Illuminate\Filesystem\FilesystemAdapter;
@@ -21,11 +22,19 @@ class TranslatorService
 
     public function translate(string $locale, string $value): string
     {
-        $translations = $this->translator->loadFile(
-            $this->path("localization/i18n/$locale/OrcaSlicer_$locale.po")
-        );
+        return $this->translations($locale)->find(null, $value)?->getTranslation() ?? $value;
+    }
 
-        return $translations->find(null, $value)?->getTranslation() ?? $value;
+    protected function translations(string $locale): Translations
+    {
+        return $this->translator->loadFile(
+            $this->getLocaleFilePath($locale)
+        );
+    }
+
+    protected function getLocaleFilePath(string $locale): string
+    {
+        return $this->path("localization/i18n/$locale/OrcaSlicer_$locale.po");
     }
 
     protected function path(string $filename): string
