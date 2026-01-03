55272eb2452cf6ebee9bec1c3c820111f0a00826
Andrey Helldar <helldar@dragon-code.pro>
2026-01-03 23:15:07 +0300

    Extend `WithColor` trait: add `colorMap` support with default hex values and update `color` method for hex initialization.

--- app/Concerns/WithColor.php
+++ app/Concerns/WithColor.php
@@ -10,8 +10,20 @@ trait WithColor
 {
     protected array $colors = [];
 
+    protected array $colorMap = [
+        'Yellow' => '#FFFF00',
+        'Green'  => '#00FF00',
+        'Blue'   => '#0000FF',
+        'Grey'   => '#808080',
+        'White'  => '#FFFFFF',
+        'Gold'   => '#FFCF40',
+    ];
+
     protected function color(string $name): Color
     {
-        return $this->colors[$name] ??= Color::firstOrCreate(['title' => $name]);
+        return $this->colors[$name] ??= Color::firstOrCreate([
+            'title' => $name,
+            'hex'   => $this->colorMap[$name] ?? '#000000',
+        ]);
     }
 }
