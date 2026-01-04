570bff4424a542927523867b490e5aea9701fecd
Andrey Helldar <helldar@dragon-code.pro>
2026-01-04 16:14:32 +0300

    Remove unused `SharedData` interface from `index.d.ts` for cleanup.

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -3,15 +3,6 @@ export interface Auth
     user: User;
 }
 
-export interface SharedData
-{
-    name: string;
-    quote: { message: string; author: string };
-    auth: Auth;
-
-    [key: string]: unknown;
-}
-
 export interface User
 {
     id: number;
